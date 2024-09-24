7b3d8992e461fef78d3fd004dbd4bcdf0a85eeb2
Nikita <tyumin000@gmail.com>
2024-09-24 17:01:49 +0300

    feat: add entrypoint

---
+++ cmd/sso/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"go-grpc-sso/internal/config"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+}
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module go-grpc-sso
+
+go 1.22.4
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/Goose47/go-grpc-sso.protos v0.0.0-20240924135313-4fc5f328de21 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
