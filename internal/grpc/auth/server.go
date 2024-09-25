506406863789fef4051fc388e0480e2e8279b2cf
Nikita <tyumin000@gmail.com>
2024-09-25 15:05:49 +0300

    feat: add handlers

---
+++ internal/grpc/auth/server.go
@@ -0,0 +1,87 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	ssov1 "github.com/Goose47/go-grpc-sso.protos/gen/go/sso"
+	"go-grpc-sso/internal/services/auth"
+	"go-grpc-sso/internal/storage"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+}
+
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
+}
+
+func (s *serverAPI) Login(
+	ctx context.Context,
+	in *ssov1.LoginRequest,
+) (*ssov1.LoginResponse, error) {
+	// todo validate another way. add phone registration?
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+	if in.AppId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "app id is required")
+	}
+
+	token, err := s.auth.Login(ctx, in.Email, in.Password, int(in.AppId))
+
+	if err != nil {
+		if errors.Is(err, auth.ErrInvalidCredentials) {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid credentials")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to login")
+	}
+
+	return &ssov1.LoginResponse{Token: token}, nil
+}
+
+func (s *serverAPI) Register(
+	ctx context.Context,
+	in *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	if in.Email == "" {
+		return nil, status.Error(codes.InvalidArgument, "email is required")
+	}
+	if in.Password == "" {
+		return nil, status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, in.Email, in.Password)
+
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			return nil, status.Error(codes.AlreadyExists, "user already exists")
+		}
+
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
+
+	return &ssov1.RegisterResponse{UserId: userID}, nil
+}
