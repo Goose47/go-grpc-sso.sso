3259cef581005ce2fa18936af5161eb1e61d17ef
Nikita <tyumin000@gmail.com>
2024-09-28 13:53:35 +0300

    test: add test suite and register_login happy path test

---
+++ tests/suite/suite.go
@@ -0,0 +1,46 @@
+package suite
+
+import (
+	"context"
+	ssov1 "github.com/Goose47/go-grpc-sso.protos/gen/go/sso"
+	"go-grpc-sso/internal/config"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"net"
+	"strconv"
+	"testing"
+)
+
+type Suite struct {
+	*testing.T
+	Cfg        *config.Config
+	AuthClient ssov1.AuthClient
+}
+
+func New(t *testing.T) (context.Context, *Suite) {
+	t.Helper()
+	t.Parallel()
+
+	cfg := config.MustLoad()
+
+	ctx, cancelCtx := context.WithTimeout(context.Background(), cfg.GRPC.Timeout)
+
+	t.Cleanup(func() {
+		t.Helper()
+		cancelCtx()
+	})
+
+	grpcAddress := net.JoinHostPort(cfg.GRPC.Host, strconv.Itoa(cfg.GRPC.Port))
+	cc, err := grpc.NewClient(grpcAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		t.Fatalf("grpc connection failed: %v", err)
+	}
+
+	authClient := ssov1.NewAuthClient(cc)
+
+	return ctx, &Suite{
+		T:          t,
+		Cfg:        cfg,
+		AuthClient: authClient,
+	}
+}
